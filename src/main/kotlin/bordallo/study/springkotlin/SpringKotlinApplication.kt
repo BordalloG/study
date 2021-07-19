4bc79c1aa9be383cae4115964f9624361ce623f9
bordallog <bordallo.guilherme@hotmail.com>
2021-07-19 08:32:05 -0300

    feat : add first files by spring initializer

---
+++ src/main/kotlin/bordallo/study/springkotlin/SpringKotlinApplication.kt
@@ -0,0 +1,11 @@
+package bordallo.study.springkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringKotlinApplication>(*args)
+}
