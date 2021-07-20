2468bae8c709c55d132847ddcee8ae1002fd7820
bordallog <bordallo.guilherme@hotmail.com>
2021-07-20 08:58:26 -0300

    feat: spring initialize + hello controller with basic auth

---
+++ src/main/kotlin/com/study/security/controllers/HelloController.kt
@@ -0,0 +1,10 @@
+package com.study.security.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+    @GetMapping("hello")
+    fun hello(): String = "hello"
+}
